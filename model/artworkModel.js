382dd5d18000a33949b94908747c256c61b7581b
carolinasmbastos <carolinasmbastos@gmail.com>
2019-10-16 15:56:57 -0700

    Create Search artwork

--- model/artworkModel.js
+++ model/artworkModel.js
@@ -1,10 +1,16 @@
 const {connectionPool} = require("../db.js");
 
 //api to find artwork, searching by artist Name
+
+
 exports.findArtworkByArtistName = (artistName, result) => {
-    connectionPool.query("SELECT * from Artwork aw, Artist a "
-        +"where aw.idArtist = a.idArtist "
-        +"and a.name like ? ", "%"+artistName+"%", (err, res)=>{
+
+    var options = {sql: "SELECT * from Artwork artwork, Artist artist "
+    +"where artwork.idArtist = artist.idArtist "
+    +"and artist.name like ? ", nestTables: true};
+
+
+    connectionPool.query(options, "%"+artistName+"%", (err, res)=>{
 
             if (err) {
                 console.log("Error listing Artowork by Artists", err);
