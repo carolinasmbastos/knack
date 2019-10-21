2d187d2184fee756e2f994183dcbfcd88cff3ffb
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-10-20 22:17:20 -0700

    Hotfix

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,6 @@ import App from "./core/App";
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
-ReactDOM.render(<ArtworkSearchResult />, document.getElementById('artworkSearchResult'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
