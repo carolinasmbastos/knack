915af70fa6ccff406a6e045c076e20be049f2a1d
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-10-21 14:59:03 -0700

    Add components  BrowseArtworks, MonthlyArt, Footer

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Carousel from './Carousel'
+import BrowseArtworks from './BrowseArtworks'
+import MonthlyArt from './MonthlyArt'
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <Carousel />
+        <BrowseArtworks />
+        <MonthlyArt />
       </div>
     );
   }
