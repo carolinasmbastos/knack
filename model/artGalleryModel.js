a8612e3931a15a7fa6f75437cb7e9a12f4513e1e
John <johhaneshongan@gmail.com>
2019-10-21 06:18:54 -0700

    get art galleries

--- model/artGalleryModel.js
+++ model/artGalleryModel.js
@@ -19,3 +19,24 @@ exports.getArtGalleryByID = (artGalleryID, result) => {
         } 
     });
 }
+
+// Developer: John
+
+exports.getArtGalleries = (result) => {
+    let sqlQuery = 'SELECT' + '\n' +
+                        'name,' + '\n' +
+                        'description' + '\n' +
+                    'FROM' + '\n' +
+                        'ArtGallery' + '\n' +
+                    'WHERE' + '\n' +
+                        'idArtGallery  = ?'
+
+    connectionPool.query(sqlQuery,(err, res)=>{
+        if (err) {
+            console.log("Error finding the ArtGallery", err);
+            result(err, null);
+        } else {
+            result(null, res);
+        } 
+    });
+}
