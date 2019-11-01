4d4ddcbf344b85108b6010a279af28274cbb05ad
John <johhaneshongan@gmail.com>
2019-11-01 11:36:35 -0700

    Revert "monthly art component"
    This reverts commit bd44a38b2ee91f3658f90a268fa7c02722847c60.

--- client/src/home/MonthlyArt.js
+++ client/src/home/MonthlyArt.js
@@ -1,22 +1,11 @@
 import React from "react";
-import {Jumbotron, Button} from 'reactstrap';
-import './MonthlyArt.css';
 
-const MonthlyArt = (props) => {
-  return (
-    <div>
-      <h1 class="title-for-monthlyArt">MONTHLY ART</h1>
-      <Jumbotron>
-        <h1 className="display-3">MONTHLY ART SUBSCRIPTION BANNER</h1>
-        <p className="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tin-
-                cidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod </p>
-        <p className="button-subcribe">
-          <Button color="primary">SUBCRIBE</Button>
-        </p>
-      </Jumbotron>
-    </div>
-  );
-};
-
-export default MonthlyArt;
+export default class MonthlyArt extends React.Component {
+  render() {
+    return (
+      <div>
+        MonthlyArt content goes here.
+      </div>
+    );
+  }
+}
