37d72c1027d992e394a9519b0f80c8beb5c73f28
jagmeet749 <55564393+jagmeet749@users.noreply.github.com>
2019-11-15 10:14:08 -0800

    Footer v1

--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -3,9 +3,23 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>
-        Footer content goes here.
-      </div>
+      
+       <div class = "footer-main-dev">
+         <div class = "logo">
+       <img className="logo" width="100%" src="knack-logo-footer.svg" alt="Card image cap" />
+      
+       </div>
+        <div class = "links">
+          <ul>
+            <li> <a href = "#">@Knack 2019</a> </li>
+            <li> <a href = "#">Contact</a> </li>
+            <li> <a href = "#">Term</a> </li>
+            <li> <a href = "#">Privacy policy</a> </li>
+            <li> <a href = "#"> Condition of rent/sale </a> </li>
+          </ul>
+        </div>
+
+       </div>
     );
   }
 }
