e9bd2aa49f5b4f78bc32dc050fb66c1f852a6c2d
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-11-17 13:42:54 -0800

    Resolve naming error related to eventController

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,40 +1,35 @@
-const eventsCache = require('./events.json');
-const axios = require('axios');
-const utils = require('./Utils');
+const eventsCache = require("./events.json");
+const axios = require("axios");
+const utils = require("./Utils");
 
 exports.getEvents = (req, res) => {
+  // EventBride API doen't allow subsequent calls within minutes, so to prevent error on the FE,
+  // we are reading previously saved data in Json object (events.json) and using it in case of API call failure
+  let events = eventsCache;
 
-    // EventBride API doen't allow subsequent calls within minutes, so to prevent error on the FE,
-    // we are reading previously saved data in Json object (events.json) and using it in case of API call failure
-    let events = eventsCache;
+  // Query events within 10km from Vancouver and from the subcategories
+  // 5008 -> 'Fine Art'
+  // 5011 -> 'Sculpture'
+  // 5012 -> 'Painting'
+  // 5013 -> 'Design'
 
+  let url =
+    "https://www.eventbriteapi.com/v3/events/search/?location.address=vancouver&location.within=10km&expand=venue&subcategories=5008%2C5011%2C5012%2C5013&token=DPEHG42NN6YYGTZP6CXZ";
 
-    // Query events within 10km from Vancouver and from the subcategories 
-    // 5008 -> 'Fine Art'
-    // 5011 -> 'Sculpture'
-    // 5012 -> 'Painting' 
-    // 5013 -> 'Design'
-
-    let url = 'https://www.eventbriteapi.com/v3/events/search/?location.address=vancouver&location.within=10km&expand=venue&subcategories=5008%2C5011%2C5012%2C5013&token=DPEHG42NN6YYGTZP6CXZ'
-
-
-
-    console.log(url)
-    axios.get(url)
-        .then(result => {
-            console.log("success")
-            console.log(result)
-            let pos = utils.randomPos(result.data.events.length);
-            res.send(result.data.events.slice(pos, pos+3))
-        })
-        .catch(err => {
-
-            console.log("error "+ err)
-            //console.log("error "+ (err.response !==undefined ? err.response.status : " no response code"))
-            console.log("Using cached copy")
-            let pos = utils.randomPos(events.events.length);
-            res.send(events.events.slice(pos, pos+3))
-        })
-
-    
-}
+  console.log(url);
+  axios
+    .get(url)
+    .then(result => {
+      console.log("success");
+      console.log(result);
+      let pos = utils.randomPos(result.data.events.length);
+      res.send(result.data.events.slice(pos, pos + 3));
+    })
+    .catch(err => {
+      console.log("error " + err);
+      //console.log("error "+ (err.response !==undefined ? err.response.status : " no response code"))
+      console.log("Using cached copy");
+      let pos = utils.randomPos(events.events.length);
+      res.send(events.events.slice(pos, pos + 3));
+    });
+};
--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-let eventController = require('../controllers/EventController')
-
+let eventController = require("../controllers/eventController");
 
 //Developer: Carol
-// EndPoint to fetch all the events from EventBride 
-router.get('/events', eventController.getEvents)
+// EndPoint to fetch all the events from EventBride
+router.get("/events", eventController.getEvents);
 
 exports.eventRouter = router;
