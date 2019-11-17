955b5a9984719954c00d574f6189143eae46dc04
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-11-17 13:07:13 -0800

    Heroku configs

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-bodyParser = require('body-parser');
+bodyParser = require("body-parser");
+const path = require("path");
 
 app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-const {indexRouter} = require('./routes/index.js');
-app.use('/', indexRouter);
+const { indexRouter } = require("./routes/index.js");
+app.use("/api", indexRouter);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.set('port', process.env.PORT || 8080);
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-const server = app.listen(app.get('port'), ()=>{
-    console.log("Listening on port: "+ app.get('port'));
+app.set("port", process.env.PORT || 8080);
+
+const server = app.listen(app.get("port"), () => {
+  console.log("Listening on port: " + app.get("port"));
 });
