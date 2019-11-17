bf32055cfa7a6db196e8482e74438a67f1caa811
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-11-16 22:41:49 -0800

    Fetch Timelines Programatically

--- client/src/artwork/TimelineSlider.js
+++ client/src/artwork/TimelineSlider.js
@@ -1,42 +1,55 @@
 import React from "react";
-import { Container, Button, ButtonGroup } from 'reactstrap';
+import { Container, Button, ButtonGroup } from "reactstrap";
+import { getTimelines } from "./api-artwork";
 
-const styles = {
-
-}
+const styles = {};
 
 export default class TimelineSlider extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      timeline: ''
-    }
+      timelines: [],
+      timeline: 0
+    };
   }
 
+  componentDidMount = () => {
+    getTimelines()
+      .then(timelines => {
+        this.setState({
+          timelines: timelines
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   loadArtworksFromTimeline = selectedTimeline => {
-    if (this.state.timeline != selectedTimeline) {
-      this.setState({
-        timeline: selectedTimeline
-      })
-      this.props.onSelect(selectedTimeline)
-    } else {
-      this.setState({
-        timeline: ''
-      })
-      this.props.onSelect('')
-    }
-  }
-  
+    this.setState({
+      timeline: selectedTimeline
+    });
+    this.props.onSelect(selectedTimeline);
+  };
+
   render() {
     return (
-      <Container className='timelineContainer'>
+      <Container className="timelineContainer">
         {/* <img src='./img/assets/TimelineSliderArrow.png' className='timelineSliderArrow' style={{marginLeft: `${() => this.sliderArrowMargin(this.state.timeline)}%`}} alt="Timeline Slider Selector" /> */}
         <ButtonGroup>
-          <Button className={`timelineButton ${this.state.timeline == '1' ? 'selected' : ''}`} onClick={() => this.loadArtworksFromTimeline('1')}>18th Century</Button>
-          <Button className={`timelineButton ${this.state.timeline == '2' ? 'selected' : ''}`} onClick={() => this.loadArtworksFromTimeline('2')}>19th Century</Button>
-          <Button className={`timelineButton ${this.state.timeline == '3' ? 'selected' : ''}`} onClick={() => this.loadArtworksFromTimeline('3')}>20th Century</Button>
-          <Button className={`timelineButton ${this.state.timeline == '4' ? 'selected' : ''}`} onClick={() => this.loadArtworksFromTimeline('4')}>21st Century</Button>
-      </ButtonGroup>
+          {this.state.timelines.map(timeline => (
+            <Button
+              className={`timelineButton ${
+                this.state.timeline == timeline.idPeriod ? "selected" : ""
+              }`}
+              onClick={() =>
+                this.loadArtworksFromTimeline(parseInt(timeline.idPeriod))
+              }
+            >
+              {timeline.periodDescription}
+            </Button>
+          ))}
+        </ButtonGroup>
       </Container>
     );
   }
