5626929af4d1553cf2383099826c0ef3bb742d87
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-11-16 20:15:38 -0800

    Footer v2

--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -3,23 +3,33 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      
-       <div class = "footer-main-dev">
-         <div class = "logo">
-       <img className="logo" width="100%" src="knack-logo-footer.svg" alt="Card image cap" />
-      
-       </div>
-        <div class = "links">
+      <div class="footer-main-div">
+        <div class="footer-item logo">
+          <img
+            width="100%"
+            src="knack-logo-footer.svg"
+            alt="Knack Footer Logo"
+          />
+        </div>
+        {/* <div className="footer-item copyright">&copy; 2019 Knack</div> */}
+        <div class="footer-item footer-links">
           <ul>
-            <li> <a href = "#">@Knack 2019</a> </li>
-            <li> <a href = "#">Contact</a> </li>
-            <li> <a href = "#">Term</a> </li>
-            <li> <a href = "#">Privacy policy</a> </li>
-            <li> <a href = "#"> Condition of rent/sale </a> </li>
+            <li className="copyright">&copy; 2019 Knack</li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li className="desktop-footer-links">
+              <a href="#">Team</a>
+            </li>
+            <li className="desktop-footer-links">
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li className="desktop-footer-links">
+              <a href="#">Condition of Rent/Sale </a>
+            </li>
           </ul>
         </div>
-
-       </div>
+      </div>
     );
   }
 }
